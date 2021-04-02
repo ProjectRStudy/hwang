b1c6271a70a69ff0f87c57bdc1bc1cde1b46ce42
hyungjoon1223 <hhj961223@naver.com>
2021-04-02 14:07:14 +0900

    비밀 정보 보호구현

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key')
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hwang:abcd1234@hwang.0tg12.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
-    useNewUrlParser: true,useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(()=>console.log('monggoDB connected...'))
-  .catch(err => console.log(err))
+mongoose.connect(config.mongoURI,{
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('mongoDB connected...'))
+    .catch(err => console.log(err))
 app.get('/', (req, res) => {
-  res.send('Hello World!~~안녕하세용~')
+    res.send('Hello World!~~안녕하세용~ㅎㅎ')
+})
+
+
+app.post('/register',(req, res) => {
+    //회원 가입 할 때 필요한 정보들을 client에서 가져오면 
+    //그것들을 데이터 베이스에 넣어준다.
+    
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
